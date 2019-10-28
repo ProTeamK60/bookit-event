c872723e9de864ae8578bb47d0233e3e5f977d53
olamat <ola.mattsson@gmail.com>
2019-10-28 17:30:06 +0100

    More tests added. One test is failing because method is not implemented.

--- src/test/java/se/knowit/bookitevent/service/map/EventServiceMapImplTest.java
+++ src/test/java/se/knowit/bookitevent/service/map/EventServiceMapImplTest.java
@@ -4,15 +4,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.knowit.bookitevent.model.Event;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class EventServiceMapImplTest {
     
+    private static final Long DEFAULT_ID = 1L;
     private EventServiceMapImpl eventService;
     
     @BeforeEach
     void setUp() {
         eventService = new EventServiceMapImpl();
+        Event event = new Event();
+        event.setName("DEFAULT_EVENT");
+        event.setId(DEFAULT_ID);
+        event.setEventId("72ab7c8b-c0d5-4ab2-8c63-5cf1ad0b439b");
+        eventService.save(event);
     }
     
     @Test
@@ -23,5 +29,24 @@ class EventServiceMapImplTest {
         Event returnedEvent = eventService.save(event);
         assertNotNull(returnedEvent);
         assertNotNull(returnedEvent.getId());
+    }
+    
+    @Test
+    void savingAnExistingEventMustNotChangeItsId() {
+        Event event = new Event();
+        event.setId(10L);
+        Event returnedEvent = eventService.save(event);
+        assertEquals(event.getId(), returnedEvent.getId());
+    }
+    
+    @Test
+    void itShouldNotBePossibleToSaveANullObject() {
+        assertThrows(IllegalArgumentException.class, () -> eventService.save(null));
+    }
+    
+    @Test
+    void itShouldBePossibleToGetEventByInternalId() {
+        Event event = eventService.findById(DEFAULT_ID);
+        assertNotNull(event);
     }
 }

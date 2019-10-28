dbbe7f326daef597e39d1bac2aca226bc82d9b65
olamat <ola.mattsson@gmail.com>
2019-10-28 15:26:57 +0100

    Added unit test file for map repo

---
+++ src/test/java/se/knowit/bookitevent/repository/map/EventRepositoryMapImplTest.java
@@ -0,0 +1,12 @@
+package se.knowit.bookitevent.repository.map;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventRepositoryMapImplTest {
+    
+    @BeforeEach
+    void setUp() {
+    }
+}

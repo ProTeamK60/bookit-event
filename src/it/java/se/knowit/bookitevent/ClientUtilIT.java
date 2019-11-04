e35197688ce281a48f2ca32d4970c9ab597474f7
Ali Afshar <ali.afshar@knowit.se>
2019-11-04 14:23:34 +0100

    changed Assertions

--- src/it/java/se/knowit/bookitevent/ClientUtilIT.java
+++ src/it/java/se/knowit/bookitevent/ClientUtilIT.java
@@ -1,59 +1,57 @@
 package se.knowit.bookitevent;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
 import se.knowit.bookitevent.model.Event;
 
 public class ClientUtilIT {
-	
+
 	@Test
-	public void getEventByIdDemo() {
+	public void testGetEventById() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		RestTemplate restTemplate = new RestTemplate();
 		String url = "http://localhost:8080/api/v1/events/{id}";
-		
-		Map<String, String> par = new HashMap<>();
-        par.put("id", "72ab7c8b-c0d5-4ab2-8c63-5cf1ad0b439b");
 
+		Map<String, String> par = new HashMap<>();
+		par.put("id", "72ab7c8b-c0d5-4ab2-8c63-5cf1ad0b439b");
 		Event eve = restTemplate.getForObject(url, Event.class, par);
 		System.out.println("Id:" + eve.getId() + " : " + eve.getName() + " : " + eve.getDescription());
-		Assert.hasText(Long.toString(eve.getId()),"1");
-		Assert.hasText(eve.getDescription(), "Ett himla bra event!");
+		Assertions.assertTrue(eve.getId() == 1L);
+		Assertions.assertTrue(eve.getDescription().contentEquals("Ett himla bra event!"));
 	}
 
 	@Test
-	public void getAllEventsDemo() {
+	public void testGetAllEvents() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		RestTemplate restTemplate = new RestTemplate();
 		String url = "http://localhost:8080/api/v1/events";
-		
-        Event allEvents[] = restTemplate.getForObject(url, Event[].class);
-        
-        Assert.hasText(Long.toString(allEvents[0].getId()), "1");
-		Assert.hasText(allEvents[0].getDescription(), "Ett himla bra event!");
-		
-        Assert.hasText(Long.toString(allEvents[1].getId()),"2");
-		Assert.hasText(allEvents[1].getDescription(), "Ännu ett himla bra event!");
-		
-		Arrays.stream(allEvents).forEach(eve -> 
-		System.out.println("Id:" + eve.getId() + " : " + eve.getName() + " : " + eve.getDescription()));
 
+		Event allEvents[] = restTemplate.getForObject(url, Event[].class);
+		Assertions.assertEquals(allEvents.length, 2);
+
+		Assertions.assertTrue(Arrays.stream(allEvents).map(e -> e.getId()).anyMatch(id -> id.equals(2L)));
+
+		Assertions.assertTrue(Arrays.stream(allEvents).map(e -> e.getId()).anyMatch(id -> id.equals(1L)));
+
+		Assertions.assertTrue(Arrays.stream(allEvents).map(e -> e.getId()).anyMatch(id -> id.equals(2L)));
+
+		Assertions.assertTrue(Arrays.stream(allEvents).map(e -> e.getDescription())
+				.anyMatch(des -> des.equals("Ett himla bra event!")));
+		Assertions.assertTrue(Arrays.stream(allEvents).map(e -> e.getDescription())
+				.anyMatch(des -> des.equals("Ännu ett himla bra event!")));
+		Arrays.stream(allEvents).forEach(
+				eve -> System.out.println("Id:" + eve.getId() + " : " + eve.getName() + " : " + eve.getDescription()));
 	}
-	
-	/*
-	public static void main(String args[]) {
-		ClientUtil util = new ClientUtil();
-		util.getAllEventsDemo();
-		util.getEventByIdDemo();
-	}*/
+
 }

7f97e1764429f575bdf2282f2e207b360acca9b0
ola.mattsson <ola.mattsson@gmail.com>
2019-11-18 14:39:59 +0100

    Try to reduce number of jobs to publish to docker hub

--- build_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-cp .mvn/wrapper/settings.xml ~/.m2/settings.xml
-echo "<settingsSecurity><master>${maven_security_master}</master></settingsSecurity>" > ~/.m2/settings-security.xml
-./mvnw compile jib:build -B -DskipTests=true \
-              -Dbuild.number=${CIRCLE_BUILD_NUM} \
-              -Dcommit.hash=${CIRCLE_SHA1} \
-              -Dcircle.workflow.guid=${CIRCLE_WORKFLOW_ID} \
-              -Dbuild.user=${CIRCLE_PROJECT_USERNAME} \
-              -Dbuild.repo=${CIRCLE_PROJECT_REPONAME}

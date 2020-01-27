680e9eec10ecd1a1c09f31eec427632f31b4b23f
ola.mattsson <ola.mattsson@gmail.com>
2020-01-27 13:20:36 +0100

    Fix autowire problem

--- src/main/java/se/knowit/bookitevent/configuration/BookitSpringConfiguration.java
+++ src/main/java/se/knowit/bookitevent/configuration/BookitSpringConfiguration.java
@@ -2,16 +2,18 @@ package se.knowit.bookitevent.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import se.knowit.bookitevent.dto.TimeSupport;
 import se.knowit.bookitevent.dto.TimeSupportImpl;
 import se.knowit.bookitevent.repository.EventRepository;
-import se.knowit.bookitevent.repository.EventRepositoryMapImpl;
+import se.knowit.bookitevent.repository.map.EventRepositoryMapImpl;
 
 @Configuration
 public class BookitSpringConfiguration {
 
     @Bean
-    public EventRepository mapBasedServiceImplementation() {
+    @Primary
+    public EventRepository mapBasedRepository() {
         return new EventRepositoryMapImpl();
     }
 

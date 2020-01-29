3586786985eaec23f5b9407176b8c4aeb734f1da
ola.mattsson <ola.mattsson@gmail.com>
2020-01-29 17:09:54 +0100

    Remove jpa and database dependencies

--- src/main/java/se/knowit/bookitevent/model/Event.java
+++ src/main/java/se/knowit/bookitevent/model/Event.java
@@ -2,21 +2,14 @@ package se.knowit.bookitevent.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.time.Instant;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 @Data
-@Entity
 public class Event {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private UUID eventId;
 	private String name;

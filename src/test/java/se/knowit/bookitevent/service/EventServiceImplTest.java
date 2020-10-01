2171dab65a1d7a3aa5a98708b2bb62a71ccbe150
aideenStrKnowit <aideen.strandsson@knowit.se>
2020-10-01 11:39:18 +0200

    removed spaces

--- src/test/java/se/knowit/bookitevent/service/EventServiceImplTest.java
+++ src/test/java/se/knowit/bookitevent/service/EventServiceImplTest.java
@@ -28,18 +28,18 @@ class EventServiceImplTest {
 
     @InjectMocks
     private EventServiceImpl eventService;
-    
+
     private final Event invalidNewEvent = new Event();
     private final EventDTO invalidNewDto = new EventDTO();
-    
+
     private Event validNewEvent;
     private EventDTO validNewDTO;
-    
+
     private Event forUpdateEvent;
     private EventDTO forUpdateDto;
     private EventDTO forUpdateWithChangeDto;
     private Event forUpdateWithChangeEvent;
-    
+
     @BeforeEach
     void setUp() {
         EventMapper mapper = new EventMapper();
@@ -47,13 +47,13 @@ class EventServiceImplTest {
         validNewDTO.setName("A test event");
         validNewDTO.setEventStart(Instant.parse("1970-01-02T01:00:00.000Z").toEpochMilli());
         validNewEvent = mapper.fromDTO(validNewDTO);
-    
+
         forUpdateDto = new EventDTO();
         forUpdateDto.setName(validNewDTO.getName());
         forUpdateDto.setEventStart(validNewDTO.getEventStart());
         forUpdateDto.setEventId(EVENT_ID);
         forUpdateEvent = mapper.fromDTO(forUpdateDto);
-        
+
         forUpdateWithChangeDto = new EventDTO();
         forUpdateWithChangeDto.setName(forUpdateDto.getName());
         forUpdateWithChangeDto.setEventStart(forUpdateDto.getEventStart());
@@ -70,28 +70,28 @@ class EventServiceImplTest {
         assertTrue(result.getEventId().isPresent());
         assertEquals(forUpdateEvent.getEventId(), result.getEventId().get());
     }
-    
+
     @Test
     void updatingExistingEventWithChangesShouldReturnOutcomeUpdated() {
         when(eventRepository.findByEventId(eq(forUpdateWithChangeEvent.getEventId()))).thenReturn(Optional.of(forUpdateEvent));
         when(eventRepository.save(eq(forUpdateWithChangeEvent))).thenReturn(forUpdateWithChangeEvent);
-        
+
         var result = eventService.createOrUpdate(forUpdateWithChangeDto);
         assertEquals(UPDATED, result.getOutcome());
         assertTrue(result.getEventId().isPresent());
         assertEquals(forUpdateWithChangeEvent.getEventId(), result.getEventId().get());
     }
-    
+
     @Test
     void attemptToSaveExistingEventWithoutChangesShouldReturnOutcomeNoChange() {
         when(eventRepository.findByEventId(eq(forUpdateEvent.getEventId()))).thenReturn(Optional.of(forUpdateEvent));
-        
+
         var result = eventService.createOrUpdate(forUpdateDto);
         assertEquals(NO_CHANGE, result.getOutcome());
         assertTrue(result.getEventId().isPresent());
         assertEquals(forUpdateEvent.getEventId(), result.getEventId().get());
     }
-    
+
     @Test
     void savingInvalidEventShouldReturnOutcomeFailed() {
         when(eventRepository.save(eq(invalidNewEvent))).thenThrow(IllegalArgumentException.class);
